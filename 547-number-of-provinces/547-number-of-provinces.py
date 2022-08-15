94757aa2ef5ed195218e866a3cc3100b4cec40c5
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-08-15 16:34:18 +0300

    Time: 216 ms (84.81%), Space: 14.3 MB (53.56%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,44 @@
+class UnionFind:
+    def __init__(self,size):
+        self.parent ={i:i for i in range(size)}
+        self.rank = {i: 0 for i in range(size)}
+        self.number_province = size
+        
+        
+        # self.rank = [1] * size
+    def find(self,v):
+        
+        #path compression
+        if self.parent[v] == v:
+            return v
+        self.parent[v] = self.find(self.parent[v])
+        
+        return self.parent[v]
+    
+    def union(self,x,y):
+        xp,yp = self.find(x),self.find(y)
+        
+        #merge by rank
+        if xp != yp:
+            self.number_province -= 1
+            if self.rank[xp]  < self.rank[yp]:
+                xp, yp = yp, xp
+                
+            self.parent[yp] = xp
+            
+            if self.rank[xp] == self.rank[yp]:
+                self.rank[xp] +=1 
+    
+    def answer(self):
+        return self.number_province
+    
+    
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        uf = UnionFind(len(isConnected))
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[0])):
+                if i != j and isConnected[i][j] == 1:
+                    uf.union(i,j)
+        
+        return uf.answer()

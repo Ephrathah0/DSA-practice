745788ffce50b5b98cde1164b61f6dcaebe22f58
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-10-04 11:52:21 +0300

    Time: 65 ms (54.15%), Space: 14.1 MB (31.57%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -0,0 +1,24 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        lookup = {}
+        def fun(i,j, chance):
+            if (i,j,chance) in lookup:
+                return lookup[(i,j,chance)]
+            
+            if i > j:
+                return 0
+            
+            if chance == 1:
+                # player 1 gets to pick
+                y =  max(nums[i] + fun(i+1, j, 2), nums[j] + fun(i, j-1, 2))
+            else:
+                # player 2 gets to pick
+                y =  min(fun(i+1, j, 1), fun(i,j-1, 1))
+                
+            lookup[(i,j,chance)] = y
+            return y
+            
+        max_p1_score = fun(0, len(nums)-1, 1)
+        if max_p1_score >= sum(nums)/2:
+            return True
+        return False

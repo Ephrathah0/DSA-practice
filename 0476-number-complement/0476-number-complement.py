a47b25a3f687754233935f01e8a72864bd9e11aa
Ephrathah <85620950+Ephrathah0@users.noreply.github.com>
2022-11-02 11:13:11 +0300

    Time: 41 ms (80.51%), Space: 14 MB (9.52%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        answer = 0
+        for i in range(len(bin(num))-2):
+            if num&(1<<i) == 0:
+                answer+=1<<i
+        return answer
